668ce5db3201991f2526c49503ae60d05c4032d5
Styfal <dragonlink999@gmail.com>
2025-01-10 16:28:26 +0900

    New EuroMatch Dataframe

--- main.R
+++ main.R
@@ -36,4 +36,21 @@ Euro <- Euro %>%
 View(Euro)
 
 EuroMatch <- dt[dt$tournament %in% c("UEFA Euro"), ]
+EuroMatch <- EuroMatch %>%
+  mutate(
+    
+    win_rating = case_when(
+      home_score > away_score ~ 1,   # Home team wins
+      home_score < away_score ~ 0,   # Away team wins
+      TRUE ~ 2                       # Tie
+    ),
+    
+    match_result = case_when(
+      home_score > away_score ~ home_team,  # Home team wins
+      home_score < away_score ~ away_team,  # Away team wins
+      TRUE ~ "tie"                          # Tie
+    )
+  )
+
 View(EuroMatch)
+
